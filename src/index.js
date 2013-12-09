9150a95f5fb82e9099b5cc17529294fed4c43287
Thomas Rampelberg <thomas@saunter.org>
2013-12-09 15:33:24 -0800

    basic style

--- src/index.js
+++ src/index.js
@@ -22,13 +22,12 @@
   };
 
   var extend = function(obj) {
-    each(Array.prototype.slice.call(arguments, 1),
-      function(source) {
-        if (source) {
-          for (var prop in source) {
-            obj[prop] = source[prop];
-          }
+    each(Array.prototype.slice.call(arguments, 1), function(source) {
+      if (source) {
+        for (var prop in source) {
+          obj[prop] = source[prop];
         }
+      }
     });
     return obj;
   };
